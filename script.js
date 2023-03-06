65c75a3217f8683aa27fcda0374c1555a0197925
Viktoria Mazhukina <zebra-vika@mail.ru>
2023-03-06 22:24:04 +0300

    create form field filling function

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const EditButton = document.querySelector('.profile__edit-button');
 const CloseButton = document.querySelector('.popup__close-button');
 
 const popupEditProfile = document.querySelector('.popup');
+const popupContainer =popupEditProfile.querySelector('.popup__container')
+const formElement = popupContainer.querySelector('.popup__admin');
+
+const nameInput = document.querySelector('.popup__item_el_name-user');
+const jobInput = document.querySelector('.popup__item_el_profession');
+
+const userNameProfile = document.querySelector('.profile__title');
+const jobProfile = document.querySelector('.profile__subtitle');
 
 function OpenModalBox(popup) {
   popup.classList.toggle('popup_opened');
@@ -11,5 +19,10 @@ function CloseModalBox(popup) {
   popup.classList.toggle('popup_opened');
 }
 
-EditButton.addEventListener('click', () => (OpenModalBox(popupEditProfile)));
+function copyEditProfileData(username, profession) {
+  nameInput.value = username.textContent;
+  jobInput.value = profession.textContent;
+}
+
+EditButton.addEventListener('click', () => (OpenModalBox(popupEditProfile), copyEditProfileData(userNameProfile, jobProfile)));
 CloseButton.addEventListener('click', ()=> CloseModalBox(popupEditProfile));
